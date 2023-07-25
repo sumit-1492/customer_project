a82a830b0a9399d020aedafda80504b6048e54cf
sumit-1492 <sumitlala99@gmail.com>
2023-07-25 06:44:27 +0530

    utils and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Customer_personality_analysis_project
+## Customer_personality_analysis_project
+
+# workflow of the project
+1. update config.yaml
+2. update params.yaml
+3. update entity
+4. update configuration manager in src config
+5. update components
+6. update pipeline
+7. update main.py
+8. update app.py
---
+++ src/customerpersonality/utils/common.py
@@ -0,0 +1,36 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from customerpersonality.logging import logging
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+## @ensure_annotations wiil take care of the arguments pass to the methods if we pass wrong datatypes then it 
+## will throw an error
+
+@ensure_annotations
+def read_yaml(path_to_yaml_file:str) -> ConfigBox:
+
+    try:
+        with open(path_to_yaml_file) as file:
+            yaml_content = yaml.safe_load(file)
+            logging.info(f"yaml file: {path_to_yaml_file} read successfully")
+            return ConfigBox(yaml_content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories:list,verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logging.info(f"Directory created at:{path}")
+
+@ensure_annotations
+def get_file_size(path:Path) -> str:
+    file_size_in_kb = round(os.path.getsize(path)/1024)
+    return f"filesize approximately: ~ {file_size_in_kb} KB"
